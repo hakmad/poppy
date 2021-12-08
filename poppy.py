ab2d03bf5e1bd5cdaa27da403b600ce4e1736d02
hakmad <88987013+hakmad@users.noreply.github.com>
2021-12-08 03:22:06 +0000

    Added simple scan function to poppy

--- poppy.py
+++ poppy.py
@@ -1,17 +1,25 @@
 import socket
 
 
-target_ip = "127.0.0.1"
+def scan(host, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.settimeout(0.5)
+
+    connection_result = client_socket.connect_ex((host, port))
+
+    if connection_result == 0:
+        print("Port open: {}".format(port))
+    else:
+        print("Port closed/filtered: {}".format(port))
+        print("Connection error: {}".format(connection_result))
+
+    client_socket.close()
+
+target_ip = socket.gethostbyname("scanme.nmap.org")
 
 print("Starting scan on: {}".format(target_ip))
 
-for port in range(1, 1023):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection_result = client_socket.connect_ex((target_ip, port))
-
-    if (connection_result == 0):
-        print("Port open: {}".format(port))
-
-    client_socket.close()
+for port in range(1, 65536):
+    scan(target_ip, port)
 
 print("Scan completed")
